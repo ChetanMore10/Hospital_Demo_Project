8783e56af775246f87675d2c8bf47388cc244b76
ChetanMore10 <chetanmore0713@gmail.com>
2026-05-20 21:55:55 +0530

    Initial commit DB Connection Established

---
+++ src/main/java/com/hospital/Hospital_Demo_Project/HospitalDemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.hospital.Hospital_Demo_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalDemoProjectApplication.class, args);
+        System.err.println("Application run Successfully..!");
+	}
+
+}
---
+++ src/test/java/com/hospital/Hospital_Demo_Project/HospitalDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.Hospital_Demo_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
