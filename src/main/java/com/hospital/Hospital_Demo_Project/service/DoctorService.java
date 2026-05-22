bb30ff35eecdda3717d4d46905bfa2b3131180b5
ChetanMore10 <chetanmore0713@gmail.com>
2026-05-22 17:50:42 +0530

    - I created Doctor Crude APIs Implemented

--- src/main/java/com/hospital/Hospital_Demo_Project/service/DoctorService.java
+++ src/main/java/com/hospital/Hospital_Demo_Project/service/DoctorService.java
@@ -3,9 +3,14 @@ package com.hospital.Hospital_Demo_Project.service;
 import com.hospital.Hospital_Demo_Project.entity.Doctor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface DoctorService {
 
     Doctor addDoctor(Doctor doctor);
+    List<Doctor> getAllDoctor();
+    Doctor updateDoctor(Doctor doctor, Long doctorId);
+    String deleteDoctor(Long doctorId);
 
 }
--- src/main/java/com/hospital/Hospital_Demo_Project/serviceImpl/DoctorServiceImpl.java
+++ src/main/java/com/hospital/Hospital_Demo_Project/serviceImpl/DoctorServiceImpl.java
@@ -6,6 +6,8 @@ import com.hospital.Hospital_Demo_Project.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DoctorServiceImpl implements DoctorService {
 
@@ -15,5 +17,31 @@ public class DoctorServiceImpl implements DoctorService {
     @Override
     public Doctor addDoctor(Doctor doctor) {
         return doctorRepo.save(doctor);
+    }
+
+    @Override
+    public List<Doctor> getAllDoctor() {
+        return doctorRepo.findAll();
+    }
+
+    @Override
+    public Doctor updateDoctor(Doctor doctor, Long doctorId) {
+        Doctor currentDoctor = doctorRepo.findById(doctorId)
+                .orElseThrow(()->new RuntimeException("Doctor Not Found with :" + doctorId));
+        currentDoctor.setDoctorName(doctor.getDoctorName());
+        currentDoctor.setEmail(doctor.getEmail());
+        currentDoctor.setPatients(doctor.getPatients());
+        currentDoctor.setExperience(doctor.getExperience());
+        currentDoctor.setSpecialization(doctor.getSpecialization());
+        currentDoctor.setContactNumber(doctor.getContactNumber());
+        return doctorRepo.save(currentDoctor);
+    }
+
+    @Override
+    public String deleteDoctor(Long doctorId) {
+       doctorRepo.findById(doctorId)
+                .orElseThrow(()-> new RuntimeException("Doctor Not Available with :" + doctorId));
+        doctorRepo.deleteById(doctorId);
+        return "Doctor Deleted Successfully...!";
     }
 }
